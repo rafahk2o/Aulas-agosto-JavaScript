1ecbe7bc85fdb72b789ca7766a387bfa53878cb3
rafahk2o@gmail.com <rafaelk2o@gmail.com>
2022-08-03 21:52:43 -0300

    commit 2

--- Aula 10  - JavaScript/js/script.js
+++ Aula 10  - JavaScript/js/script.js
@@ -1,7 +1,7 @@
-let retorno = $(".retorno")
-let retorno2 = $(".retorno2")
-let retorno3 = $(".retorno3")
-let retorno4 = $(".retorno4")
+let retorno = $("#retorno")
+let retorno2 = $("#retorno2")
+let retorno3 = $("#retorno3")
+let retorno4 = $("#retorno4")
 
 let campoCep = $("#cep");
 
@@ -13,7 +13,7 @@ campoCep.blur(function () {
         buscaCep(cep)
 
     } else {
-        alert("Digite o CEP!!!")
+        Swal.fire('Digite o CEP!')
     }
 
 })
@@ -26,22 +26,27 @@ function buscaCep(cep) {
         },
 
         success: (resposta) => {
+
+            Swal.fire(
+                'CEP encontrado!',
+                'O endereço buscado já está em tela!',
+                'success')
             console.log(resposta);
             let dados = `<p>CEP: ${resposta.cep}</p>`;
             retorno.html(dados);
             let dados2 = `<p>Logradouro: ${resposta.logradouro}</p>`;
-            retorno2.html(dados2);   
+            retorno2.html(dados2);
             let dados3 = `<p>Localidade: ${resposta.localidade}</p>`;
-            retorno3.html(dados3);   
+            retorno3.html(dados3);
             let dados4 = `<p>IBGE: ${resposta.ibge}</p>`;
-            retorno4.html(dados4);   
+            retorno4.html(dados4);
         },
 
-        
+
 
         error: () => {
             console.log("Deu ruim!!")
         }
-        
+
     })
 }
