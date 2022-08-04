1299694c860cc3c10a2499cb4aeb7047928ebc2b
rafahk2o@gmail.com <rafaelk2o@gmail.com>
2022-08-03 21:24:10 -0300

    asdfg

---
+++ Aula 10  - JavaScript/js/script.js
@@ -0,0 +1,47 @@
+let retorno = $(".retorno")
+let retorno2 = $(".retorno2")
+let retorno3 = $(".retorno3")
+let retorno4 = $(".retorno4")
+
+let campoCep = $("#cep");
+
+campoCep.blur(function () {
+    var cep = $(this).val();
+
+    if (cep != "") {
+        console.log(cep);
+        buscaCep(cep)
+
+    } else {
+        alert("Digite o CEP!!!")
+    }
+
+})
+
+function buscaCep(cep) {
+    $.ajax({
+        url: `https://viacep.com.br/ws/${cep}/json/`,
+        beforeSend: () => {
+            console.log("pesquisando...")
+        },
+
+        success: (resposta) => {
+            console.log(resposta);
+            let dados = `<p>CEP: ${resposta.cep}</p>`;
+            retorno.html(dados);
+            let dados2 = `<p>Logradouro: ${resposta.logradouro}</p>`;
+            retorno2.html(dados2);   
+            let dados3 = `<p>Localidade: ${resposta.localidade}</p>`;
+            retorno3.html(dados3);   
+            let dados4 = `<p>IBGE: ${resposta.ibge}</p>`;
+            retorno4.html(dados4);   
+        },
+
+        
+
+        error: () => {
+            console.log("Deu ruim!!")
+        }
+        
+    })
+}
