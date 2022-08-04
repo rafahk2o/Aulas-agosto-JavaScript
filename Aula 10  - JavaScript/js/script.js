fed9a2a24fc75d7da82da3be332ef5753e5c5a35
rafahk2o <49276128+rafahk2o@users.noreply.github.com>
2022-08-04 12:56:08 -0300

    modified

--- Aula 10  - JavaScript/js/script.js
+++ Aula 10  - JavaScript/js/script.js
@@ -1,7 +1,4 @@
-let retorno = $("#retorno")
-let retorno2 = $("#retorno2")
-let retorno3 = $("#retorno3")
-let retorno4 = $("#retorno4")
+
 
 let campoCep = $("#cep");
 
@@ -32,14 +29,11 @@ function buscaCep(cep) {
                 'O endereço buscado já está em tela!',
                 'success')
             console.log(resposta);
-            let dados = `<p>CEP: ${resposta.cep}</p>`;
-            retorno.html(dados);
-            let dados2 = `<p>Logradouro: ${resposta.logradouro}</p>`;
-            retorno2.html(dados2);
-            let dados3 = `<p>Localidade: ${resposta.localidade}</p>`;
-            retorno3.html(dados3);
-            let dados4 = `<p>IBGE: ${resposta.ibge}</p>`;
-            retorno4.html(dados4);
+            $("#retorno").val(resposta.cep);
+            $("#retorno2").val(resposta.logradouro);
+            $("#retorno3").val(resposta.localidade);
+            $("#retorno4").val(resposta.ibge);
+           
         },
 
 
